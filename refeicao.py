4f77a201b9ec34c164233f58cf6f502e6a309cbd
Lucival junior <lucival.sjunior@gmail.com>
2020-07-11 00:17:28 -0300

    Configuracao de relatorio sococo

--- refeicao.py
+++ refeicao.py
@@ -1,10 +1,11 @@
+#importe dos pacotes
 import streamlit as st
 import pandas as pd
 from PIL import Image
-import numpy as np
 
+#carrega logo do empresa
 image = Image.open('sococo-logo.png')
-st.sidebar.image(image,width=250 , caption='Relatório de Refeições', use_column_width=True)
+st.sidebar.image(image,width=200 , caption='Relatório de Refeições', use_column_width=True)
 
 # Add a checkbox to the sidebar:
 cafe = st.sidebar.checkbox('Café')
@@ -12,39 +13,44 @@ almoco = st.sidebar.checkbox('Almoco')
 lanche = st.sidebar.checkbox('Lanche')
 ceia = st.sidebar.checkbox('Ceia')
 
-if cafe:
-    number = st.number_input('Insert a number')
-    st.write('The current number is ', number)
+#variaveis de arquivo saida
+out_file = "primeira_limpeza.txt"
+out_file2 = "segunda_limpeza.txt"
 
-out_file = "modelo1.txt"
-out_file2 = "saida.txt"
+#busca para limpeza
 search_for = "21"
 search_for2 = "Estabelecimento:"
 
-uploaded_file2 = st.file_uploader("Choose a CSV file", type="txt", encoding="cp1252")
-if uploaded_file2 is not None:
- colunas = [5,11,28,12,7,7]
- data = pd.read_fwf(uploaded_file2, widths=colunas, header=None)
- data.to_csv('novo.txt', index=False)
- st.write(data)
-
+#recebe o arquivo de texto e formata em 06 colunas
 uploaded_file = st.file_uploader("Escolha seu arquivo: ", type="txt", encoding="windows-1252")
+colunas = [5,11,28,12,7,7]
+
 if uploaded_file is not None:
+# se o arquivo foi carregado, le o arquivo e gera um novo arquivo formatado
+    data = pd.read_fwf(uploaded_file, widths=colunas, header=None)
+    data.to_csv('arquivo_sem_tratamento.txt', index=False)
+    # st.write(data)
+
+# Inicia a primeira limpeza do arquivo gerado acima.
+# Gera um novo aquivo com a primeira limpeza concluida.
     with open(out_file, 'w', encoding="windows-1252") as out_f:
-        with open('novo.txt', "r") as in_f:
+        with open('arquivo_sem_tratamento.txt', "r") as in_f:
             for line in in_f:
                 if search_for in line:
                     out_f.write(line)
 
+# Inicia a segunda limpeza do arquivo gerado acima.
+# Gera o arquivo final limpo
     with open(out_file2, 'w', encoding="windows-1252") as out_f:
-        with open('modelo1.txt', "r", encoding="windows-1252") as in_f:
+        with open('primeira_limpeza.txt', "r", encoding="windows-1252") as in_f:
             for line in in_f:
                 if search_for2 in line:
                     print('nao tem')
                 else:
                   out_f.write(line)
 
-    df = pd.read_csv('saida.txt', sep=',')
+#carrega arquivo da segunda limpeza e gera o arquivo final limpo para mostrar na tela
+    df = pd.read_csv('segunda_limpeza.txt', sep=',')
     df.columns = ['Empresa', 'Matricula', 'Funcionario', 'Data', 'Hora', 'Refeicao']
-    df.to_csv('modelagem.txt', index=False)
+    df.to_csv('arquivo_limpo.txt', index=False)
     st.write("Shape do Dataset", df)
